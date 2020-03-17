0a94fcbe62d3df8d95a633d7fe2ec1779eafc322
titanmet <titanmet@mail.ru>
2020-03-17 21:44:21 +0300

    before

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,24 +1,34 @@
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
+    private final double EPS = 1e-9;
+    private static Vector2D v1;
+
+    @BeforeClass
+    public static void createNewVector() {
+        v1 = new Vector2D();
+    }
+
     @Test
     public void newVectorShouldHaveZeroLenght() {
-        Vector2D v1 = new Vector2D(); // action
+//        Vector2D v1 = new Vector2D(); // action , create @Before , close @After
         // assertion
         // 1e-9 = 0.0000000001
-        Assert.assertEquals(0, v1.lenght(), 1e-9);
+        Assert.assertEquals(0, v1.lenght(), EPS);
     }
 
     @Test
     public void newVectorShouldHaveZeroX() {
-        Vector2D x1 = new Vector2D();
-        Assert.assertEquals(0, x1.getX(), 1e-9);
+//        Vector2D x1 = new Vector2D();
+        Assert.assertEquals(0, v1.getX(), EPS);
     }
 
     @Test
     public void newVextorShouldZeroY() {
-        Vector2D y1 = new Vector2D();
-        Assert.assertEquals(0, y1.getX(), 1e-9);
+//        Vector2D y1 = new Vector2D();
+        Assert.assertEquals(0, v1.getY(), EPS);
     }
 }
